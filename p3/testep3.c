fd8fe86d005ea24af184eb687b93f2dcaed655bf
Madara763 <clashroyalemadara763@gmail.com>
2025-04-19 15:09:08 -0300

    ini p3

--- p3/testep3.c
+++ p3/testep3.c
@@ -2,39 +2,41 @@
 // Prof. Carlos A. Maziero, DINF UFPR
 // Versão 1.5 -- Março de 2023
 
-// Teste da gestão básica de tarefas
+// Teste do task dispatcher e escalonador FCFS
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "ppos.h"
 
-#define MAXTASK 1000
-
-task_t task ;
+task_t Pang, Peng, Ping, Pong, Pung ;
 
 // corpo das threads
-void BodyTask (void * arg)
+void Body (void * arg)
 {
-   printf ("Estou na tarefa %5d\n", task_id()) ;
+   int i ;
+
+   printf ("%s: inicio\n", (char *) arg) ;
+   for (i=0; i<5; i++)
+   {
+      printf ("%s: %d\n", (char *) arg, i) ;
+      task_yield ();
+   }
+   printf ("%s: fim\n", (char *) arg) ;
    task_exit (0) ;
 }
 
 int main (int argc, char *argv[])
 {
-   int i ;
-
    printf ("main: inicio\n");
 
    ppos_init () ;
 
-   // inicia MAXTASK tarefas, ativando cada uma apos sua criacao
-   for (i=0; i<MAXTASK; i++)
-   {
-     task_init (&task, BodyTask, NULL) ;
-     task_switch (&task) ;
-   }
+   task_init (&Pang, Body, "    Pang") ;
+   task_init (&Peng, Body, "        Peng") ;
+   task_init (&Ping, Body, "            Ping") ;
+   task_init (&Pong, Body, "                Pong") ;
+   task_init (&Pung, Body, "                    Pung") ;
 
    printf ("main: fim\n");
-
    task_exit (0);
 }
